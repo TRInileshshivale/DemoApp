d575d7e12f08ede8bc0ff389d0574e92437f6209
Nilesh Shivale <n.shivale@tricentis.com>
2018-03-21 11:30:25 +0530

    Initial  Commit

---
+++ CITestingExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace CITestingExample
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string strFirstName = Console.ReadLine();
+                string strLastName = Console.ReadLine();
+                Console.WriteLine(Add(strFirstName, strLastName));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+
+        public static string Add(string strFirstName, string strLastName)
+        {
+            if (string.IsNullOrEmpty(strFirstName) || string.IsNullOrEmpty(strLastName))
+            {
+                throw new Exception("Input Values Can not be Null or Empty...");
+            }
+            Regex regex = new Regex(@"^([a-zA-Z]+\s)*[a-zA-Z]+$");
+            if (!(regex.IsMatch(strFirstName) || regex.IsMatch(strLastName)))
+            {
+                throw new Exception("Value Should Only Accept Characters and Whitespaces..");
+            }
+
+            return string.Concat(strFirstName, " ", strLastName);
+        }
+    }
+}
---
+++ UnitTestUsingNUnit/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using CITestingExample;
+using NUnit.Framework;
+using System;
+
+namespace UnitTestUsingNUnit
+{
+    [TestFixture]
+   public class ProgramTests
+    {
+        [TestCase("Harshad","Patel",ExpectedResult ="Harshad Patel",Description ="Name always have a Only Characters and WhiteSpace",TestName = "Positive Test Scenario One")]
+        [TestCase("", "", ExpectedResult = "Input Values Can not be Null or Empty...", Description = "NULL Or Empty Name is not good..",TestName = "Negative Test Scenario One")]
+        [TestCase("h123sf", "gh456", ExpectedResult = "Value Should Only Accept Characters and Whitespaces..", Description = "Name With Numbers is not good..",TestName= "Negative Test Scenario Two")]
+        public string Add(string strFirstName, string strLastName)
+        {
+            string actualResult = string.Empty;
+            try
+            {
+                actualResult = Program.Add(strFirstName, strLastName);
+            }
+            catch (Exception ex)
+            {
+                actualResult = ex.Message;
+            }
+            return actualResult;
+        }
+
+    }
+}
